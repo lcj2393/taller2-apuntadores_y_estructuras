870e6589d9e745fc59e1fd4a996b41a8df984f3b
Luis Jordan <lc.jordan93@gmail.com>
2017-03-02 14:31:05 -0500

    Ejercicio 5 iniciado

--- 5-ESTRUC_NOTAS_Y_N-ESTUDIANTES.cpp
+++ 5-ESTRUC_NOTAS_Y_N-ESTUDIANTES.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
-#define almn 3
 
 using namespace std;
+
+int almn=0;
 
 struct alumno
 {
@@ -11,44 +12,48 @@ struct alumno
     char sexo;
     int edad;
     float promedio;
-};alumno estudiante[almn];
+};alumno estudiante[0];
 
 struct calificaciones
 {
     float nota1;
     float nota2;
     float nota3;
-};calificaciones notas[almn];
+};calificaciones notas[0];
 
 int main()
 {
-    for(int i=0;i<almn;i++)
+    int n;
+    while(n!=0)
     {
-    cout<<"\n\t\tREGISTRO ESTUDIANTE "<<i+1<<"\n";
-    cout<<"\n***********************************************\n";
-    cout<<"\nDigite Nombre: ";
-        cin.getline(estudiante[i].nombre,30);
-        cin.ignore(256,'\n');
+        cout<<"\n\t\tREGISTRO ESTUDIANTE "<<almn+1<<"\n";
+        cout<<"\n***********************************************\n";
+        cout<<"\nDigite Nombre: ";
+            cin.getline(estudiante[almn].nombre,30);
+            cin.ignore(256,'\n');
 
-    cout<<"\nDigite Sexo: ";
-        cin>>estudiante[i].sexo;
-        cin.ignore(256,'\n');
+        cout<<"\nDigite Sexo: ";
+            cin>>estudiante[almn].sexo;
+            cin.ignore(256,'\n');
 
-    cout<<"\nDigite Edad: ";
-        cin>>estudiante[i].edad;
+        cout<<"\nDigite Edad: ";
+            cin>>estudiante[almn].edad;
 
-    cout<<"\n\t\tREGISTRO DE NOTAS\n\n";
-    cout<<"\nNota 1: ";
-        cin>>notas[i].nota1;
-    cout<<"\nNota 2: ";
-        cin>>notas[i].nota2;
-    cout<<"\nNota 3: ";
-        cin>>notas[i].nota3;
-    cout<<"\n***********************************************\n";
+        cout<<"\n\t\tREGISTRO DE NOTAS\n\n";
+        cout<<"\nNota 1: ";
+            cin>>notas[almn].nota1;
+        cout<<"\nNota 2: ";
+            cin>>notas[almn].nota2;
+        cout<<"\nNota 3: ";
+            cin>>notas[almn].nota3;
+        cout<<"\n***********************************************\n";
+            estudiante[almn].promedio=((notas[almn].nota1+notas[almn].nota2+notas[almn].nota3)/3);
 
-        estudiante[i].promedio=((notas[i].nota1+notas[i].nota2+notas[i].nota3)/3);
+        cout<<"\nDesea seguir registrando estudiantes? (1<SI> o 0<NO>): ";
+            cin>>n;
+            if(n==1)
+            {   almn++;}
     }
-    system("cls");
 
     cout<<"\nEl registro de estudiantes es:\n\n";
     for(int i=0;i<almn;i++)
