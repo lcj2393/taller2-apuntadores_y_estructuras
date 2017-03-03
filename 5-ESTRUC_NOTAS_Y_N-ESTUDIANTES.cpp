346633e9371bcbfef5a6b20ee30d3606cd963846
Luis Jordan <lc.jordan93@gmail.com>
2017-03-03 14:29:01 -0500

    Ejercicio 5 finalizado

--- 5-ESTRUC_NOTAS_Y_N-ESTUDIANTES.cpp
+++ 5-ESTRUC_NOTAS_Y_N-ESTUDIANTES.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#define almn 100
 
 using namespace std;
-
-int almn=0;
 
 struct alumno
 {
@@ -12,51 +11,52 @@ struct alumno
     char sexo;
     int edad;
     float promedio;
-};alumno estudiante[0];
+};alumno estudiante[almn];
 
 struct calificaciones
 {
     float nota1;
     float nota2;
     float nota3;
-};calificaciones notas[0];
+};calificaciones notas[almn];
 
 int main()
 {
     int n;
+    int c_almn=0;
     while(n!=0)
     {
-        cout<<"\n\t\tREGISTRO ESTUDIANTE "<<almn+1<<"\n";
+        cout<<"\n\t\tREGISTRO ESTUDIANTE "<<c_almn+1<<"\n";
         cout<<"\n***********************************************\n";
         cout<<"\nDigite Nombre: ";
-            cin.getline(estudiante[almn].nombre,30);
+            cin.getline(estudiante[c_almn].nombre,30);
             cin.ignore(256,'\n');
 
         cout<<"\nDigite Sexo: ";
-            cin>>estudiante[almn].sexo;
+            cin>>estudiante[c_almn].sexo;
             cin.ignore(256,'\n');
 
         cout<<"\nDigite Edad: ";
-            cin>>estudiante[almn].edad;
+            cin>>estudiante[c_almn].edad;
 
         cout<<"\n\t\tREGISTRO DE NOTAS\n\n";
         cout<<"\nNota 1: ";
-            cin>>notas[almn].nota1;
+            cin>>notas[c_almn].nota1;
         cout<<"\nNota 2: ";
-            cin>>notas[almn].nota2;
+            cin>>notas[c_almn].nota2;
         cout<<"\nNota 3: ";
-            cin>>notas[almn].nota3;
+            cin>>notas[c_almn].nota3;
         cout<<"\n***********************************************\n";
-            estudiante[almn].promedio=((notas[almn].nota1+notas[almn].nota2+notas[almn].nota3)/3);
+            estudiante[c_almn].promedio=((notas[c_almn].nota1+notas[c_almn].nota2+notas[c_almn].nota3)/3);
 
         cout<<"\nDesea seguir registrando estudiantes? (1<SI> o 0<NO>): ";
             cin>>n;
             if(n==1)
-            {   almn++;}
+            {   c_almn++;}
     }
 
     cout<<"\nEl registro de estudiantes es:\n\n";
-    for(int i=0;i<almn;i++)
+    for(int i=0;i<=c_almn;i++)
     {
         cout<<"\n\t\tESTUDIANTE "<<i+1<<"\n";
         cout<<"\n***********************************************\n";
