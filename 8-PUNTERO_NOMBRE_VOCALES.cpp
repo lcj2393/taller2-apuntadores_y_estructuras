c7458f23fd11dfd2b3edd68be10914be2bb2c110
Luis Jordan <lc.jordan93@gmail.com>
2017-03-07 15:24:28 -0500

    Ejericicio 8 Punteros Iniciado

--- 8-PUNTERO_NOMBRE_VOCALES.cpp
+++ 8-PUNTERO_NOMBRE_VOCALES.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 int main()
 {
-    char nombre[10],*pnombre=nombre;
+    char nombre[30],*pnombre=nombre;
 
     pedir_nombre(pnombre);
     c_vocales(pnombre);
@@ -22,41 +22,46 @@ int main()
 void pedir_nombre(char *pnombre){
 
     cout<<"\nDigite Nombre: ";
-    cin>>*pnombre;
+    cin.getline(pnombre,30);
+    cin.ignore(256,'\n');
     cout<<"\n";
 }
 
 void c_vocales(char *p){
 
     int l_nombre;
-    int n_v=0;
+    int n_a=0,n_e=0,n_i=0,n_o=0,n_u=0,n_v;
     char c_v[2];
 
-    l_nombre=strlen(*p);
+    l_nombre=strlen(p);
 
     for(int i=0;i<(l_nombre+1);i++){
 
-        c_v[0]=*(p+i);
+        c_v[0]=*(p+1);
 
         switch(c_v[0]){
     case 'a':
-        n_v++;
+        n_a++;n_v;
         break;
     case 'e':
-        n_v++;
+        n_e++;n_v;
         break;
     case 'i':
-        n_v++;
+        n_i++;n_v;
         break;
     case 'o':
-        n_v++;
+        n_o++;n_v;
         break;
     case 'u':
-        n_v++;
+        n_u++;n_v;
         break;
         }
 
     }
-    cout<<"\nEl nombre tiene "<<l_nombre+1<<" letras.\n";
     cout<<"\nEl nombre ingresado tiene "<<n_v<<" vocales.\n";
+    cout<<"\na= "<<n_a;
+    cout<<"\ne= "<<n_e;
+    cout<<"\ni= "<<n_i;
+    cout<<"\no= "<<n_o;
+    cout<<"\nu= "<<n_u;
 }
